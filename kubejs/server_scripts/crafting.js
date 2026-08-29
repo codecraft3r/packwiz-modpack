16083a91884ae2ed0cec188b0a38d03ff4e70f2d
John Software <72947633+codecraft3r@users.noreply.github.com>
2026-08-29 15:00:01 -0700

    feat(mods): add Lootr, KubeJS Mekanism, KubeJS Create & update Mekanism circuit/alloy recipes to require Create components

--- kubejs/server_scripts/crafting.js
+++ kubejs/server_scripts/crafting.js
@@ -3,4 +3,36 @@ ServerEvents.recipes(event => {
   event.remove({ output: 'mekanism:mekasuit_bodyarmor' })
   event.remove({ output: 'mekanism:mekasuit_pants' })
   event.remove({ output: 'mekanism:mekasuit_boots' })
+
+  // --- Mekanism Recipe Modifications ---
+
+  // 1. basic_control_circuit requires create:precision_mechanism instead of osmium ingot
+  event.replaceInput({ output: 'mekanism:basic_control_circuit' }, 'mekanism:ingot_osmium', 'create:precision_mechanism')
+  event.replaceInput({ output: 'mekanism:basic_control_circuit' }, '#c:ingots/osmium', 'create:precision_mechanism')
+  event.replaceInput({ output: 'mekanism:basic_control_circuit' }, '#neoforge:ingots/osmium', 'create:precision_mechanism')
+  event.replaceInput({ output: 'mekanism:basic_control_circuit' }, '#forge:ingots/osmium', 'create:precision_mechanism')
+
+  event.remove({ output: 'mekanism:basic_control_circuit' })
+  event.shaped('mekanism:basic_control_circuit', ['RCR'], {
+    R: 'minecraft:redstone',
+    C: 'create:precision_mechanism'
+  })
+  if (event.recipes && event.recipes.mekanism) {
+    event.recipes.mekanism.infusing('mekanism:basic_control_circuit', 'create:precision_mechanism', 'mekanism:redstone', 20)
+  }
+
+  // 2. alloy_infused requires create:brass_ingot instead of copper ingot
+  event.replaceInput({ output: 'mekanism:alloy_infused' }, 'minecraft:copper_ingot', 'create:brass_ingot')
+  event.replaceInput({ output: 'mekanism:alloy_infused' }, '#c:ingots/copper', 'create:brass_ingot')
+  event.replaceInput({ output: 'mekanism:alloy_infused' }, '#neoforge:ingots/copper', 'create:brass_ingot')
+  event.replaceInput({ output: 'mekanism:alloy_infused' }, '#forge:ingots/copper', 'create:brass_ingot')
+
+  event.remove({ output: 'mekanism:alloy_infused' })
+  event.shaped('mekanism:alloy_infused', ['RCR'], {
+    R: 'minecraft:redstone',
+    C: 'create:brass_ingot'
+  })
+  if (event.recipes && event.recipes.mekanism) {
+    event.recipes.mekanism.infusing('mekanism:alloy_infused', 'create:brass_ingot', 'mekanism:redstone', 10)
+  }
 })
